b379de7348fdb3ca71d25bdfae4690460fe4d7df
pranav A waghmare <129757297+Pranwagh@users.noreply.github.com>
2024-08-31 12:19:33 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 echo "i am that"
 echo "123"
 echo "456"
-aws ec2 describe-instances --region ap-south-1
+#aws ec2 describe-instances --region ap-south-1
+aws ec2 describe-instances --query "Reservations[*].Instances[*].{Name:Tags[?Key=='Name']|[0].Value,InstanceId:InstanceId}" --output table --region <your-region>
+
